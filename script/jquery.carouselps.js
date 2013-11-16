c6bbf33d2253fa8921b6f872e861dbc572fe8dfd
Leon Slater <admin@lpslater.co.uk>
2013-11-16 14:16:25 +0000

    css transition callbacks continued - slight cleanup

--- script/jquery.carouselps.js
+++ script/jquery.carouselps.js
@@ -9,18 +9,18 @@
     $.fn.carouselps = function (options) {
 
         var defaults = {
-            fade: false, // if true, will set continuous and use_css3 to false
-            continuous: true,
-            auto_slide: true,
-            auto_direction: 'next', // 'next' or 'prev'
-            arrow_nav: true,
-            bottom_nav: true,
-            use_css3: true,
-            swipe: true,
-			responsive: true,
-            adjust_height: false, // fade sets this to true by default
-            slideChangeSpeed: 7000,
-            animateSpeed: 500
+            fade: false,              // use fade or slide transition mode: if true, will set continuous and use_css3 to false
+            continuous: true,         // determines whether the slide loop is continuous
+            auto_slide: true,         // whether or not the carousel will animate automatically
+            auto_direction: 'next',   // auto-animate direction: 'next' or 'prev'
+            arrow_nav: true,          // control whether or not arrow navigation renders
+            bottom_nav: true,         // control whether or not bottom navigation renders
+            use_css3: true,           // if supported, control whether or not the slide transitions use the translate3d css3 property
+            swipe: true,              // enable/disable touch swipe capability
+            responsive: true,         // determine if the slides should alter width on resize - their width is set to the slider's parent width
+            adjust_height: true,      // whether or not the slider should adjust height based on the active slide: fade sets this to true by default
+            slideChangeSpeed: 2500,   // the time interval between the carousel's auto-animations
+            animateSpeed: 500         // the animation speed between slides
         };
 
         options = $.extend({}, defaults, options);
@@ -28,25 +28,26 @@
         return this.each(function () {
 
             var $slider = $(this),
-				$sliderItems = $slider.children('li'),
-				$sliderItemFirst = $slider.children('li:first-child'),
-				$sliderItemLast = $slider.children('li:last-child'),
-				$sliderItemCurrent = $slider.find('.current'),
-				$sliderStartClone,
-				$sliderEndClone,
-				$sliderParent,
-				$sliderWrapper,
-				$bottomNav,
-				$bottomNavItem,
-				bottomNavClickIndex,
-				isAnimating = css3support = false,
-				animateDirection,
-				youtubeExists = $slider.find('iframe').length,
-				iframe = $slider.find('iframe'),
-				youtubePlaying = false,
-				itemMinHeight,
-				heightsArray = [],
-				animProp, cssPrefix, slideTimer;
+                $sliderItems = $slider.children('li'),
+                $sliderItemFirst = $slider.children('li:first-child'),
+                $sliderItemLast = $slider.children('li:last-child'),
+                $sliderItemCurrent = $slider.find('.current'),
+                $sliderStartClone,
+                $sliderEndClone,
+                $sliderParent,
+                $sliderWrapper,
+                $bottomNav,
+                $bottomNavItem,
+                bottomNavClickIndex,
+                isAnimating = false,
+				css3support = false,
+                animateDirection,
+                youtubePlaying = false,
+                hovering = false,
+                animProp, cssPrefix, slideTimer,
+                isMobile = /android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent.toLowerCase()),
+                touch = isMobile ? ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch : false,
+                clickEvent = touch ? 'touchend' : 'click';
 
             var carouselps = {
                 init: function () {
@@ -92,15 +93,13 @@
 
                 use_css3: function () {
                     var div = document.createElement('div'),
-						props = ['WebkitPerspective', 'MozPerspective', 'OPerspective', 'msPerspective'];
+                        props = ['WebkitPerspective', 'MozPerspective', 'OPerspective', 'msPerspective'];
                     for (var i in props) {
                         if (div.style[props[i]] != undefined) {
                             css3support = true;
                             cssPrefix = props[i].replace('Perspective', '').toLowerCase();
-                            animProp = '-' + cssPrefix + '-transform';
-                            $slider.css('-' + cssPrefix + '-transition-property', '-' + cssPrefix + '-transform');
-                            $slider.css('-' + cssPrefix + '-transition-duration', '0s');
-                            $slider.css('-' + cssPrefix + '-transition-timing-function', 'ease-out');
+                            animProp = '-' + cssPrefix + '-transform';							
+                            $slider.css('-' + cssPrefix + '-transition', '-' + cssPrefix + '-transform 0s ease-out');
                             return true;
                         }
                     }
@@ -124,8 +123,8 @@
                 continuous: function () {
                     $sliderItemFirst.clone(true).insertAfter($sliderItemLast).addClass('clone').removeClass('current');
                     $sliderItemLast.clone(true).insertBefore($sliderItemFirst).addClass('clone');
-                    $sliderStartClone = $slider.find('li:first-child');
-                    $sliderEndClone = $slider.find('li:last-child');
+                    $sliderStartClone = $sliderItemFirst.prev('li');
+                    $sliderEndClone = $sliderItemLast.next('li');
                     $sliderItems = $slider.children('li');
                 },
 
@@ -157,21 +156,15 @@
                         }
                         $sliderItemCurrent = $slider.find('.current');
                         if (options.use_css3 && css3support) {
-                            $slider.css('-' + cssPrefix + '-transition-duration', options.animateSpeed / 1000 + 's');
-                            $slider.css(animProp, 'translate3d(' + $sliderItemCurrent.position().left * -1 + 'px, 0, 0)');
-							$slider.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-								carouselps.after_anim();
-							});
+                            $slider.css('-' + cssPrefix + '-transition-duration', options.animateSpeed / 1000 + 's')
+                               .css(animProp, 'translate3d(' + $sliderItemCurrent.position().left * -1 + 'px, 0, 0)')
+                               .one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', carouselps.after_anim);
                         } else {
                             if (options.fade) {
                                 $sliderItems.fadeOut(options.animateSpeed);
-                                $sliderItemCurrent.fadeIn(options.animateSpeed, function(){
-									carouselps.after_anim();
-								});
+                                $sliderItemCurrent.fadeIn(options.animateSpeed, carouselps.after_anim);
                             } else {
-                                $slider.animate({ marginLeft: $sliderItemCurrent.position().left * -1 }, options.animateSpeed, function(){
-									carouselps.after_anim();
-								});
+                                $slider.animate({ marginLeft: $sliderItemCurrent.position().left * -1 }, options.animateSpeed, carouselps.after_anim);
                             }
                         }
                         if (options.bottom_nav) {
@@ -210,30 +203,35 @@
                         $sliderItemCurrent = $slider.find('.current');
                     }
                     isAnimating = false;
-                    if (options.auto_slide) {
-                        clearTimeout(slideTimer);
-                        carouselps.auto_slide();
+                    if (options.auto_slide && !hovering) {
+	                    if (slideTimer){
+                            clearTimeout(slideTimer);
+                        }
+                        animateDirection = options.auto_direction;
+	                    slideTimer = setTimeout(carouselps.animate, options.slideChangeSpeed);
                     }
                 },
 
                 auto_slide: function () {
                     animateDirection = options.auto_direction;
                     slideTimer = setTimeout(carouselps.animate, options.slideChangeSpeed);
-                    $sliderWrapper.unbind('hover').hover(function () {
+                    $sliderWrapper.hover(function () {
+						hovering = true;
                         if (slideTimer){
                             clearTimeout(slideTimer);
                         }
                     }, function () {
+						hovering = false;
                         if (!youtubePlaying) {
-                            animateDirection = "next";
+                            animateDirection = options.auto_direction;
                             slideTimer = setTimeout(carouselps.animate, options.slideChangeSpeed);
                         }
                     });
                 },
 
                 swipe: function () {
-                    var startX = startY = movementXOffset = movementYOffset = swipeDistanceX = swipeDistanceY = null,
-						sliding = 0, scrolling = true;
+                    var startX = null, startY = null, movementXOffset = null, movementYOffset = null, swipeDistanceX = null, swipeDistanceY = null,
+                        sliding = 0, scrolling = true;
                     $slider.on({
                         touchstart: slideStart,
                         touchmove: slide,
@@ -241,33 +239,31 @@
                     });
                     function slideStart(event) {
                         if ((!event.originalEvent.touches[1]) && (!isAnimating)) {
-							varReset();
+                            varReset();
                             if (sliding == 0) {
                                 sliding = 1;
-								scrolling = true;
                                 startX = event.originalEvent.touches[0].clientX;
                                 startY = event.originalEvent.touches[0].clientY;
                             }
                         }
                     }
                     function slide(event) {
-						
-						swipeDistanceX = event.originalEvent.touches[0].clientX - startX;
-						swipeDistanceY = event.originalEvent.touches[0].clientY - startY;
-						scrolling = Math.abs(swipeDistanceX) < Math.abs(swipeDistanceY);
+                        swipeDistanceX = event.originalEvent.touches[0].clientX - startX;
+                        swipeDistanceY = event.originalEvent.touches[0].clientY - startY;
+                        scrolling = Math.abs(swipeDistanceX) < Math.abs(swipeDistanceY);
 							
-						if (!scrolling){
-							event.preventDefault();
-							if (options.auto_slide) {
-								clearTimeout(slideTimer);
-							}
-							sliding = 2;
-							if (css3support) {
-								movementXOffset = ($sliderItemCurrent.position().left * -1) + swipeDistanceX;
-								$slider.css('-' + cssPrefix + '-transition-duration', '0s');
-								$slider.css(animProp, 'translate3d(' + movementXOffset + 'px,0,0)');
-							}
-						}
+                        if (!scrolling){
+                            event.preventDefault();
+                            if (options.auto_slide) {
+                                clearTimeout(slideTimer);
+                            }
+                            sliding = 2;
+                            if (css3support) {
+                                movementXOffset = ($sliderItemCurrent.position().left * -1) + swipeDistanceX;
+                                $slider.css('-' + cssPrefix + '-transition-duration', '0s')
+                                    .css(animProp, 'translate3d(' + movementXOffset + 'px,0,0)');
+                            }
+                        }
                     }
                     function slideEnd(event) {
                         if (sliding == 2) {
@@ -281,51 +277,56 @@
                             carouselps.animate();
                         }
                     }
-					function varReset() {
-						startX = startY = movementXOffset = movementYOffset = swipeDistanceX = swipeDistanceY = null;
-						sliding = 0; 
-						scrolling = true;
-					}
+                    function varReset() {
+                        startX = null, startY = null, movementXOffset = null, movementYOffset = null, swipeDistanceX = null, swipeDistanceY = null,
+                        sliding = 0, scrolling = true;
+                    }
                 },
 
                 arrow_nav: function () {
                     $sliderParent.append('<ul class="carouselps-nav"><li class="prev"><a><</a></li><li class="next"><a>></a></li></ul>');
                     $sliderBottomNavItem = $sliderParent.find('.carouselps-nav li');
-                    $sliderBottomNavItem.click(function () {
+                    $sliderBottomNavItem.bind(clickEvent, function () {
                         animateDirection = $(this).attr('class');
                         carouselps.animate();
                     });
                 },
 
                 bottom_nav: function () {
-                    $sliderWrapper.append('<ul class="carouselps-nav-bottom"/>');
+                    if (!$sliderWrapper.find('.carouselps-nav-bottom').length){
+                        $sliderWrapper.append('<ul class="carouselps-nav-bottom"/>');
+                    }
                     $bottomNav = $sliderWrapper.find('.carouselps-nav-bottom');
                     $sliderItems.each(function () {
-                        $bottomNav.append('<li><a/></li>');
+                        $bottomNav.append('<li><a></a></li>');
                     });
                     $bottomNavItem = $bottomNav.find('li');
                     $bottomNav.find('li:first-child').addClass('current');
-                    $bottomNavItem.click(function () {
-                        bottomNavClickIndex = parseInt($(this).index());
+                    $bottomNavItem.bind(clickEvent, function () {
+                        bottomNavClickIndex = $(this).index();
                         animateDirection = "bottom";
                         carouselps.animate();
                     });
                 },
 
                 youtube_exists: function () {
-                    iframe = $slider.find('iframe');
-                    iframe.each(function () {
-                        var aspectRatio = $(this).height() / $(this).width() * 100 + "%";
-                        $(this).wrap("<div class='video-wrapper' style='padding-bottom: " + aspectRatio + "' />");
-                        $(this).css({
-                            'position': 'absolute',
-                            'height': '100%',
-                            'width': '100%',
-                            'top': '0',
-                            'left': '0'
-                        });
+                    var $iframes = $slider.find('iframe');
+                    $iframes.each(function () {
+                        var $iframe = $(this);
+                        if ($iframe.attr('src').toLowerCase().indexOf('youtube') > -1) {
+                            var itemWidth = !isNaN(parseInt($iframe.attr('width'), 10)) ? 
+                                    parseInt($iframe.attr('width'), 10) : $iframe.width(),
+                                itemHeight = (this.tagName.toLowerCase() === 'object' || 
+                                    ($iframe.attr('height') && !isNaN(parseInt($iframe.attr('height'), 10)))) ?
+                                    parseInt($iframe.attr('height'), 10) : $iframe.height(),
+                                aspectRatio = (itemHeight / itemWidth) * 100 + "%";
+
+                            $iframe.removeAttr('width').removeAttr('height')
+                                .wrap("<div class='video-wrapper' style='padding-bottom: " + aspectRatio + "' />")
+                                .css({'position': 'absolute', 'height': '100%', 'width': '100%', 'top': '0', 'left': '0'});
+                        }
                     });
-                    iframe.hover(function () {
+                    $iframes.hover(function () {
                         if (slideTimer) {
                             clearTimeout(slideTimer);
                         }
@@ -337,7 +338,7 @@
             carouselps.init();
 
             $(window).bind('load', function () {
-                if (youtubeExists) {
+                if ($slider.find('iframe').length) {
                     carouselps.youtube_exists();
                 }
                 if (options.adjust_height) {
@@ -345,21 +346,20 @@
                 }
             });
 			
-			if (options.responsive){
-				var timer,
-					isMobile = /android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent.toLowerCase()),
+            if (options.responsive){
+                var timer,
                     orientationSupport = isMobile ? window.hasOwnProperty('orientation') : false,
-					resizeEvent = orientationSupport ? 'orientationchange' : 'resize';
-					
-				$(window).bind(resizeEvent, function () {
-					if (timer) {
-						clearTimeout(timer);
-					}
-					timer = setTimeout(function () {
-						carouselps.calcs();
-					}, 100);
-				});
-			}
+                    resizeEvent = orientationSupport ? 'orientationchange' : 'resize';
+
+                $(window).bind(resizeEvent, function () {
+                    if (timer) {
+                        clearTimeout(timer);
+                    }
+                    timer = setTimeout(function () {
+                        carouselps.calcs();
+                    }, 100);
+                });
+            }
         });
 
     };
